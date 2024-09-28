b309e60d754bcb65e1d7e04440f01bbfd5227ad9
Konstantin-Natev <konstantin@nolimit.software>
2024-09-29 00:16:14 +0300

    feat: create create service for email sending

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -3,8 +3,21 @@ import styles from "./contact-form.module.css";
 import skillsData from '../../data/skills.json';
 import socialContactsData from '../../data/social-contacts.json';
 import { CustomButton } from "../CustomComponents/CustomButton/CustomButton";
+import emailjs from 'emailjs-com';
+import { useRef } from "react";
 
 export const ContactForm = () => {
+    const form = useRef(null);
+
+    const sendEmail = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        emailjs.sendForm('service_stclons', 'template_0t96lvf', form.current, "heMBs2eh2ICqWQ-gn")
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
 
     return (
         <Grid2 className={styles.root}>
@@ -14,7 +27,7 @@ export const ContactForm = () => {
                     <Typography className={styles.subheadingText}>get in touch</Typography>
                 </Grid2>
                 <Grid2 className={styles.mainTitleSection}>
-                    <Typography  className={styles.mainTitle}>Drop me a line to say hello or talk about opportunities</Typography>
+                    <Typography className={styles.mainTitle}>Drop me a line to say hello or talk about opportunities</Typography>
                 </Grid2>
             </Stack>
 
@@ -22,7 +35,7 @@ export const ContactForm = () => {
                 <Grid item xs={3} className={styles.skillsAndContactsContainer}>
                     <Stack className={styles.skillsContainer}>
                         <Typography className={styles.subtitle}>Skills</Typography>
-                        <Divider className={styles.divider}/>
+                        <Divider className={styles.divider} />
                         <Grid2 className={styles.skillsList}>
                             {skillsData.map((skill, index) => (
                                 <Typography key={index} className={styles.skillItem}>{skill}</Typography>
@@ -32,69 +45,66 @@ export const ContactForm = () => {
 
                     <Stack className={styles.skillsContainer}>
                         <Typography className={styles.subtitle}>Social Contacts</Typography>
-                        <Divider className={styles.divider}/>
+                        <Divider className={styles.divider} />
                         <Stack>
                             {socialContactsData.map((contact, index) => (
-                                <a href={contact.url}  key={index} className={styles.linkItems}>{contact.text}</a>
+                                <a href={contact.url} key={index} className={styles.linkItems}>{contact.text}</a>
                             ))}
                         </Stack>
                     </Stack>
-
                 </Grid>
 
                 <Grid item xs={5} className={styles.formContainer}>
-                    <Grid2 marginBottom={"40px"}>
-                        <Typography className={styles.label}>Name</Typography>
-                        <TextField
-                            id={"name"}
-                            name="name"
-                            value={""}
-                            onChange={() => {}}
-                            className={styles.input}
-                            fullWidth
-                            InputProps={{
-                                classes: {
-                                    notchedOutline: styles.noFocusOutline,
-                                },
-                            }}
-                        />
+                    <form ref={form} onSubmit={sendEmail}>
+                        <Grid2 marginBottom={"40px"}>
+                            <Typography className={styles.label}>Name</Typography>
+                            <TextField
+                                id={"name"}
+                                name="name"
+                                value={""}
+                                onChange={() => {}}
+                                className={styles.input}
+                                fullWidth
+                                InputProps={{
+                                    classes: {
+                                        notchedOutline: styles.noFocusOutline,
+                                    },
+                                }}
+                            />
+                        </Grid2>
 
-                    </Grid2>
+                        <Grid2 marginBottom={"40px"}>
+                            <Typography className={styles.label}>Email</Typography>
+                            <TextField
+                                id={"email"}
+                                name="email"
+                                value={""}
+                                onChange={() => {}}
+                                className={styles.input}
+                                fullWidth
+                                InputProps={{
+                                    classes: {
+                                        notchedOutline: styles.noFocusOutline,
+                                    },
+                                }}
+                            />
+                        </Grid2>
 
-                    <Grid2 marginBottom={"40px"}>
-                        <Typography className={styles.label}>Email</Typography>
-                        <TextField
-                            id={"email"}
-                            name="email"
-                            value={""}
-                            onChange={() => {}}
-                            className={styles.input}
-                            fullWidth
-                            InputProps={{
-                                classes: {
-                                    notchedOutline: styles.noFocusOutline,
-                                },
-                            }}
-                        />
+                        <Grid2 marginBottom={"20px"}>
+                            <Typography className={styles.label}>Message</Typography>
+                            <TextareaAutosize
+                                id="message"
+                                name="message"
+                                value={""}
+                                onChange={() => {}}
+                                className={styles.textarea}
+                            />
+                        </Grid2>
 
-                    </Grid2>
-
-                    <Grid2  marginBottom={"20px"}>
-                        <Typography className={styles.label}>Message</Typography>
-                        <TextareaAutosize
-                            id="message"
-                            name="message"
-                            value={""}
-                            onChange={() => {}}
-                            className={styles.textarea}
-                        />
-
-                    </Grid2>
-
-                    <CustomButton text={"Submit Message"} variant={"outlined"} />
+                        <CustomButton text={"Submit Message"} variant={"outlined"} type="submit" />
+                    </form>
                 </Grid>
             </Grid>
-
         </Grid2>
     );
-}
+};
